3fcd612c107c0256dc611dddc37a70249eb04b31
Nghiem Ba Hieu <hi3unb@gmail.com>
2014-03-27 14:49:49 +0700

    Cap nhat: sort Staff info:Search theo Staff id

--- admin/userinfo.php
+++ admin/userinfo.php
@@ -54,7 +54,7 @@ if ($op == "op_id"){
 $sql_tim = "select staff_id,name,startdate from staff where staff_id='$tim'";
 }
 else{
-		$sql_tim = "select staff_id,name,startdate from staff where name ilike '%$tim%'";
+		$sql_tim = "select staff_id,name,startdate from staff where name ilike '%$tim%' ORDER BY staff_id ASC";
 	}
 echo '<h3>Staff Information</h3>';
                 echo '<table width="100%" border = "0">';
