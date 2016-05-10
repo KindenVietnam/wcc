980421ba3f350861e9d74dbc6baec31bc0164805
Nghiem Ba Hieu <hi3unb@gmail.com>
2016-05-10 13:12:44 +0700

    remove admin stuffs from web portion.

--- admin/del_leaves_sub.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("config.php");
-$id = $_GET['id'];
-$staffid = $_GET['staffid'];
-$sql_del = "delete from leaves where id = '$id'";
-$ketqua = pg_query($connection, $sql_del);
-header("Location:detail_user.php?id1=$staffid");
-?>
